10dbd39c0f6a9dde242568a072d27c5bb84daa59
jacky <fox0905@outlook.com>
2025-12-16 14:56:57 +0100

    fix

--- exchange-v3/src/utils/constants.template.ts
+++ exchange-v3/src/utils/constants.template.ts
@@ -13,4 +13,5 @@ export let ZERO_BD = BigDecimal.fromString("0");
 export let ONE_BD = BigDecimal.fromString("1");
 export let BI_18 = BigInt.fromI32(18);
 
+
 export let factoryContract = FactoryContract.bind(Address.fromString(FACTORY_ADDRESS));
